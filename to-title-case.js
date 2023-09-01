4aceeba78a8e0bf49c8b91c667c2f4ad1fa0560c
title-case <18871315325@163.com>
2023-09-01 17:33:01 +0800

    update

--- to-title-case.js
+++ to-title-case.js
@@ -1,15 +1,20 @@
 /* To Title Case © 2018 David Gouch | https://github.com/gouch/to-title-case */
-
+/* Edited by Siyu Wu */
 // eslint-disable-next-line no-extend-native
 String.prototype.toTitleCase = function () {
   'use strict'
-  var smallWords = /^(a|an|and|as|at|but|by|en|for|if|in|nor|of|on|or|per|the|to|v.?|vs.?|via)$/i
-  var alphanumericPattern = /([A-Za-z0-9\u00C0-\u00FF])/
-  var wordSeparators = /([ :–—-])/
+  let smallWords = /^(a|an|and|as|at|but|by|en|for|if|in|nor|of|on|or|per|the|to|v.?|vs.?|via)$/i
+  let alphanumericPattern = /([A-Za-z0-9\u00C0-\u00FF])/
+  /* note there is a capturing group, so the separators will also be included in the returned list */
+  let wordSeparators = /([ :–—-])/
   let lowerBar = /_/g;
-  let trimBeginEndPattern = /^[\s.,;:\-_!?]*([a-zA-Z0-9].*[a-zA-Z0-9])[\s.,;:\-_!?]*$/g;
+  /* regular expression: remove the space character, punctuation (.,;:!?), 
+     dash and lower bar at both ends of the string */
+  let trimBeginEndPattern = /^[\s.,;:!?_\-]*([a-zA-Z0-9].*[a-zA-Z0-9])[\s.,;:!?_\-]*$/g;
 
-  return this.replace(trimBeginEndPattern,"$1").replace(lowerBar, " ").split(wordSeparators)
+  return this.replace(trimBeginEndPattern,"$1")
+    .replace(lowerBar, " ")
+    .split(wordSeparators)
     .map(function (current, index, array) {
       if (
         /* Check for small words */
@@ -17,6 +22,8 @@ String.prototype.toTitleCase = function () {
         /* Skip first and last word */
         index !== 0 &&
         index !== array.length - 1 &&
+        /* cope the situation such as: 1. the conjugation operator */
+        array.slice(0,index-1).join('').search(/a-zA-Z/)>-1 &&
         /* Ignore title end and subtitle start */
         array[index - 3] !== ':' &&
         array[index + 1] !== ':' &&
@@ -28,7 +35,7 @@ String.prototype.toTitleCase = function () {
       }
 
       /* Ignore intentional capitalization */
-      if (current.substr(1).search(/[A-Z]|\../) > -1) {
+      if (current.substring(1).search(/[A-Z]|\../) > -1) {
         return current
       }
 
@@ -42,5 +49,7 @@ String.prototype.toTitleCase = function () {
         return match.toUpperCase()
       })
     })
-    .join('')
+    .join('') // convert the list into a string
 }
+
+console.log("1 the conjugation operator".toTitleCase())
